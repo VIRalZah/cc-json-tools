f90c04e3b5fea41660fd6a02859f9015eae2af93
Viral <129858326+VIRalZah@users.noreply.github.com>
2023-11-13 17:23:30 +0300

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+#include <cocos2d.h>
+#include <cocos-ext.h>
+#include <cocos/ui/CocosGUI.h>
+#include <cocos/scripting/deprecated/CCDeprecated.h>
+#include <cJSON/cJSON.h>
+#include <cJSON/cJSON_Utils.h>
+#include <Utils.h>
+USING_NS_CC;
+USING_NS_CC_EXT;
+using namespace cocos2d::ui;
+using namespace cocos2d::utils;
+#define ValueNull Value(Value::Null);
+#endif
